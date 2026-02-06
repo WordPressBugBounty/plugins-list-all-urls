70f3fbf0a085c3881b74114784cd9c83c4757ee9
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 04:30:48 +0000

    1.0.1

--- list-all-urls/list-all-urls.php
+++ list-all-urls/list-all-urls.php
@@ -3,12 +3,12 @@
  * Plugin Name: List all URLs
  * Plugin URI: https://jonathanbossenger.com
  * Description: Creates a page in the admin panel under Settings > List All URLs that outputs an ordered list of all of the website's published URLs.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Jonathan Bossenger
  * Author URI: https://jonathanbossenger.com
  * License: GPL v2 or higher
  * License URI: License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Tested up to: 6.8.1
+ * Tested up to: 6.8.3
  * Text Domain: list-all-urls
  */
 
@@ -113,69 +113,4 @@ function jb_lau_generate_url_list( $arguments = array() ) {
     }
 
     return $links;
-}
-
-/**
- * Check if the WordPress Feature API plugin is installed and active
- */
-if ( ! is_plugin_active( 'wp-feature-api/wp-feature-api.php' ) ) {
-	return;
-}
-
-/**
- * Register the plugin's feature using the Feature API
- */
-add_action('init', 'register_list_all_urls_feature');
-function register_list_all_urls_feature(){
-	wp_register_feature( array(
-		'id'          => 'list-all-urls/get-urls',
-		'name'        => __( 'Get Urls', 'list-all-urls' ),
-		'description' => __( 'Retrieves all the urls of the selected post type.', 'list-all-urls' ),
-		'type'        => WP_Feature::TYPE_RESOURCE,
-		'callback'    => 'jb_lau_get_urls',
-		'permission_callback' => '__return_true',
-		'output_schema' => array(
-            'type'        => 'object',
-            'properties'  => array(
-                'urls' => array(
-                    'type'        => 'array',
-                    'description' => __( 'List of URLs.', 'list-all-urls' ),
-                    'items'       => array(
-                        'type'        => 'string',
-                        'description' => __( 'URL of the post.', 'list-all-urls' ),
-                    ),
-                ),
-            ),
-        ),
-        'input_schema' => array(
-            'type'        => 'object',
-            'properties'  => array(
-                'post_type' => array(
-                    'type'        => 'string',
-                    'description' => __( 'The post type to retrieve URLs for.', 'list-all-urls' ),
-                ),
-                'limit' => array(
-                    'type'        => 'integer',
-                    'description' => __( 'The maximum number of URLs to retrieve.', 'list-all-urls' ),
-                ),
-            )
-        ),
-	) );
-}
-
-function jb_lau_get_urls( $context ){
-	$post_type = $context['post_type'] ?? 'post';
-	$limit     = $context['limit'] ?? 10;
-
-	$urls = jb_lau_generate_url_list(
-		array(
-			'post_type'      => $post_type,
-			'posts_per_page' => $limit,
-			'post_status'    => 'publish',
-		)
-	);
-
-    return array(
-        'urls' => $urls,
-    );
 }
